7d14d21e49612e7ea4efc8ede1d36842e9d6dece
Yucheoru <dbcjf182529@gmail.com>
2024-06-24 21:08:58 +0900

    new

---
+++ Docker/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('🐳 Dream Coding in Docker! 🐳');
+});
+
+app.listen(8080, () => console.log('Server is running'));
